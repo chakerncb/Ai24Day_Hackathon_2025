c9dde0b4aba821f47da8b6224bb643f0574c65db
Chaker Necibi <154261868+chakerncb@users.noreply.github.com>
2025-04-12 00:23:37 +0100

    Add Laravel Sanctum for API authentication and configure routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -14,12 +15,12 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
 
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->validateCsrfTokens(except: [
-            '/tasks/*',
-            '/tasks',
-        ]);
-    })
+    // ->withMiddleware(function (Middleware $middleware) {
+    //     $middleware->validateCsrfTokens(except: [
+    //         '/tasks/*',
+    //         '/tasks',
+    //     ]);
+    // })
 
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::prefix('/tasks')->namespace('App\Http\Controllers')->group(function () {
-    
-Route::get('/', 'TasksController@index')->name('tasks.getAll');
 
-Route::get('/{id}', 'TasksController@show')->name('tasks.getById');
-
-Route::post('/', 'TasksController@store')->name('tasks.create');
-
-Route::put('/{id}', 'TasksController@update')->name('tasks.update');
-
-Route::delete('/{id}', 'TasksController@destroy')->name('tasks.delete');
-
-});
