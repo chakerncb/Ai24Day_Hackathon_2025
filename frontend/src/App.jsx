83fcad86dcc21d8f0e8aa6479b723d8544fb86e1
Chaker Necibi <154261868+chakerncb@users.noreply.github.com>
2025-04-16 10:50:15 +0100

    feat: Implement reception staff update functionality with form handling and API integration
    feat: Create trainer dashboard with search functionality and mock data display
    
    feat: Develop user activity logs component with error handling and refresh capability
    
    feat: Add trainee management system with add, edit, and delete functionalities
    
    feat: Introduce offers and categories data structure for membership and packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,47 @@
 import "./App.css";
-import Home from "./Home";
-import { Route, Routes } from "react-router-dom";
-import Updatelist from "./Updatelist";
-import AddItem from "./AddItem";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Componenets/Login";
+import Home from "./Componenets/Home";
+import { ToastContainer, Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Admin from "./Componenets/Admin/Admin";
+import Reception from "./Componenets/Reception/Reception";
+import Trainers from "./Componenets/trainers/Trainers";
+import Users_log from "./Componenets/Users_log/Users_log";
+import Caoch from "./Componenets/Caoch/Caoch";
+import UpdateReception from "./Componenets/Reception/UpdateReception";
+
+
 function App() {
 
+
   return (
-    <main>
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        transition={Bounce}
+      />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/update/:id" element={<Updatelist/>} />
-        <Route path="/add" element={<AddItem/>} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="*" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/Admin" element={<Admin />} />
+        <Route path="/Reception" element={<Reception />} />
+        <Route path="/Trainers" element={<Trainers />} />
+        <Route path="/Caoch" element={<Caoch />} />
+        <Route path="/Update-reception/:id" element={<UpdateReception />} />
+        <Route path="/Users-log" element={<Users_log/>} />
       </Routes>
-    </main>
+    </>
   );
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8000',
-    },
-  },
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
+ 
 });
